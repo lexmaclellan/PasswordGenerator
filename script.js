c322b548d98f4c749a3041541ed0ed48af40faa1
lexrayne <lexrayne@outlook.com>
2023-05-13 22:53:20 -0300

    Added prompts and validation for character types

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passLength = prompt("Enter a length for the password (minimum 8, maximum 128");
+var passLowercase = prompt("Include lowercase characters?");
+var passUppercase = prompt("Include uppercase characters?");
+var passNumeric = prompt("Include numeric characters?");
+var passSpecial = prompt("Include special characters?");
+
+if (!passLowercase && !passUppercase && !passNumeric && !passSpecial) {
+ alert("Please include at least one character type.");
+}
